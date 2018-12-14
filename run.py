d9f09be46b6ebec12d470f887976009971d5f72d
tolgahanuzun <mail@tolgahanuzun.com>
2018-12-14 11:02:18 +0300

    Vote Class Refactoring

--- run.py
+++ run.py
@@ -1,37 +1,51 @@
 import os
+import logging
 
 import atexit
 from apscheduler.scheduler import Scheduler
 from flask import Flask
 
-from utils import Curation, get_vp, get_rc, post_vote
+from utils import Curation, Steemit
 import settings
 
 app = Flask(__name__)
 
 voted_txt = 'voted.txt'
-
-def vote_list():
-    if not get_vp(settings.username) >= settings.limit_power and get_rc < 0.1 :
-        return []
-    return Curation('function', settings.results).result
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 
-def voted_list():
-    with open(voted_txt, 'w') as file:
-        lines = file.read().splitlines()
-    return lines
+class PostVote:
 
-def voting_list(votings):
-    for voting in votings:
-        username = list(filter(lambda x: '@' in x, voting))
-        try:
-            post_vote(settings.username, username, voting, 100)
-            with open(voted_txt, "a") as f:
-                f.write("{}\n".format(voting))
-        except:
-            pass
-    print('TODO: Logger.info(Done!)')
+    def __init__(self):
+        self.vote = self.vote_list()
+        self.voted = self.voted_list()
+        self.steemit = Steemit(settings.username)
+
+    def vote_list(self):
+        if not self.steemit.get_vp(settings.username) >= settings.limit_power and self.steemit.get_rc() < 0.1 :
+            return []
+        return Curation(settings.type, settings.results).result
+
+    def voted_list(self):
+        with open(voted_txt, 'w') as file:
+            lines = file.read().splitlines()
+        return lines
+
+    def voting_list(self):
+        votings = list(set(self.vote) - set(self.voted))
+        for voting in votings:
+            username = list(filter(lambda x: '@' in x, voting))
+            try:
+                self.steemit.post_vote(username, voting, 100)
+                with open(voted_txt, "a") as f:
+                    f.write("{}\n".format(voting))
+            except:
+                pass
+        logging.info('TODO: Logger.info(Done!)')
+
+    def removed_list(self):
+        with open(voted_txt, "w") as f:
+            f.write('')
 
 def control_flow():
     cron = Scheduler(daemon=True)
@@ -39,11 +53,8 @@ def control_flow():
 
     @cron.interval_schedule(seconds=60*10)
     def job_function():
-        _vote_list = vote_list()
-        _voted_list = voted_list()
-        voting = list(set(_vote_list) - set(_voted_list))
-        voting_list(voting)
-
+        vote_commit = PostVote()
+        vote_commit.voting_list()
 
     atexit.register(lambda: cron.shutdown(wait=False))
 
@@ -54,7 +65,8 @@ def clear_list():
 
     @cron.interval_schedule(days=4)
     def job_function():
-        # link file removed.
+        vote_commit = PostVote()
+        vote_commit.removed_list()
 
     atexit.register(lambda: cron.shutdown(wait=False))
 
