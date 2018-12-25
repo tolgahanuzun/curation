a7ab224aa1036b26f4584f69ff0754bf496b7dd7
tolgahanuzun <mail@tolgahanuzun.com>
2018-12-25 10:33:14 +0300

    User model and admin configration add

--- run.py
+++ run.py
@@ -1,9 +1,19 @@
 import os
 import logging
+from datetime import datetime, timedelta
+
+
+from flask import Flask, url_for, redirect, request
+from flask_admin import base, helpers, expose
+from flask_admin.contrib import sqla
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import form, fields, validators
+from werkzeug.security import check_password_hash
 
 import atexit
 from apscheduler.scheduler import Scheduler
-from flask import Flask
+import flask_admin as admin
+import flask_login as login
 
 from utils import Curation, Steemit
 import settings
@@ -12,6 +22,121 @@ app = Flask(__name__)
 
 voted_txt = 'voted.txt'
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'steemit'
+app.config['DATABASE_FILE'] = 'tag.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + app.config['DATABASE_FILE']
+app.config['SQLALCHEMY_ECHO'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    login = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(64))
+
+    # Flask-Login integration
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+    # Required for administrative interface
+    def __unicode__(self):
+        return self.username
+
+
+# Define login and registration forms (for flask-login)
+class LoginForm(form.Form):
+    login = fields.StringField(validators=[validators.required()])
+    password = fields.PasswordField(validators=[validators.required()])
+
+    def validate_login(self, field):
+        user = self.get_user()
+
+        if user is None:
+            raise validators.ValidationError('Invalid user')
+
+        if not check_password_hash(user.password, self.password.data):
+            raise validators.ValidationError('Invalid password')
+
+    def get_user(self):
+        return db.session.query(User).filter_by(login=self.login.data).first()
+
+
+class RegistrationForm(form.Form):
+    login = fields.StringField(validators=[validators.required()])
+    email = fields.StringField()
+    password = fields.PasswordField(validators=[validators.required()])
+
+    def validate_login(self, field):
+        if db.session.query(User).filter_by(login=self.login.data).count() > 0:
+            raise validators.ValidationError('Duplicate username')
+
+
+# Initialize flask-login
+def init_login():
+    login_manager = login.LoginManager()
+    login_manager.init_app(app)
+
+    # Create user loader function
+    @login_manager.user_loader
+    def load_user(user_id):
+        return db.session.query(User).get(user_id)
+
+
+# Create customized model view class
+class MyModelView(sqla.ModelView):
+
+    def is_accessible(self):
+        return login.current_user.is_authenticated
+
+
+# Create customized index view class that handles login & registration
+class MyAdminIndexView(base.AdminIndexView):
+
+    @expose('/')
+    def index(self):
+        if not login.current_user.is_authenticated:
+            return redirect(url_for('.login_view'))
+        return super(MyAdminIndexView, self).index()
+
+    @expose('/login/', methods=('GET', 'POST'))
+    def login_view(self):
+        # handle user login
+        form = LoginForm(request.form)
+        if helpers.validate_form_on_submit(form):
+            user = form.get_user()
+            login.login_user(user)
+
+        if login.current_user.is_authenticated:
+            return redirect(url_for('.index'))
+        link = ''
+        self._template_args['form'] = form
+        self._template_args['link'] = link
+        return super(MyAdminIndexView, self).index()
+
+    @expose('/logout/')
+    def logout_view(self):
+        login.logout_user()
+        return redirect(url_for('.index'))
+    
+# Flask views
+@app.route('/admin')
+def admin():
+    return redirect(url_for('admin.login_view'))
 
 
 class PostVote:
